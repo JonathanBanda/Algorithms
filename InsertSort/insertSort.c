e500ea69420d1340518b59c84aa95734bf27c3ed
Jonathan Banda <jonathanbanda00@gmail.com>
2020-09-03 13:16:20 -0500

    Created a main to go with the function will sort the array 9 thourgh 0 in order from 0 to 9.

--- InsertSort/insertSort.c
+++ InsertSort/insertSort.c
@@ -10,11 +10,11 @@ Insert sort
 void insertSort(int A[], int N)
 {
 	int i, k, key;
-	for (i = 0; i < N; i+)
+	for (i = 0; i < N; i++)
 	{
 		key = A[i];
-		K = I - 1;
-		while((k >= 0)&&(A[K] > key))
+		k = i - 1;
+		while((k >= 0)&&(A[k] > key))
 		{
 			A[k+1] = A[k];
 			k--;
@@ -23,7 +23,23 @@ void insertSort(int A[], int N)
 	}
 }
 
+int main(void)
+{
+	int A[] = {9,8,7,6,5,4,3,2,1,0};
+	int N = sizeof(A)/sizeof(A[0]);
 
+	int i;
+
+	insertSort(A, N);
+
+	for(i = 0; i < N; i++)
+	{
+		printf("%d ", A[i]);
+	}
+
+	return 0;
+
+}
 
 
 
